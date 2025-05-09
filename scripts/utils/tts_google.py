11f39cf955c74e23d7489a23b39ce6b6db3639e9
Stephen Allen <founder@discoversoftwaresolution.com>
2025-05-09 14:39:03 -0400

    Create tts_google.py

---
+++ scripts/utils/tts_google.py
@@ -0,0 +1,16 @@
+from google.cloud import texttospeech
+import os
+
+def synthesize_speech_to_bytes(text: str) -> bytes:
+    client = texttospeech.TextToSpeechClient()
+    input_text = texttospeech.SynthesisInput(text=text)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.LINEAR16)
+
+    response = client.synthesize_speech(input=input_text, voice=voice, audio_config=audio_config)
+    return response.audio_content
